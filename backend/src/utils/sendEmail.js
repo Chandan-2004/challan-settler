84903ba6dc64b6ddb450876ffe48727e3bceb07e
Chandan-2004 <chandanyadavcbr2004@gmail.com>
2026-04-15 01:36:50 +0530

    Fix sendEmail import/export

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,14 +1,26 @@
+const resend = require("../config/resend");
+
 const DEV_EMAIL = "chandanyadavcbr2004@gmail.com";
 
-const sendEmail = (to, subject, html) => {
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: [DEV_EMAIL], // 🔥 always your email
-    subject,
-    html: `
-      <p><b>Original recipient:</b> ${to}</p>
-      <hr/>
-      ${html}
-    `,
-  });
+const sendEmail = async (to, subject, html) => {
+  try {
+    console.log("Sending email to:", DEV_EMAIL);
+
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: [DEV_EMAIL],
+      subject,
+      html: `
+        <p><b>Original recipient:</b> ${to}</p>
+        <hr/>
+        ${html}
+      `,
+    });
+
+    console.log("Email sent successfully");
+  } catch (err) {
+    console.error("Email error:", err.message);
+  }
 };
+
+module.exports = sendEmail;
